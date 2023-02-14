e67028c4d382d1d3c4dc1b9186c5890ee12befcc
hajer <hajer.zrigui@gmail.com>
2023-02-14 09:22:22 +0100

    second commit

--- src/Components/Addtask.js
+++ src/Components/Addtask.js
@@ -3,9 +3,9 @@ import React, { useState } from 'react'
 const Addtask = ({fnc}) => {
     const [newtask, setnewtask] = useState({name:"",done:false});
   return (
-    <div>
+    <div className='form'>
+      <img src='https://adniasolutions.com/wp-content/uploads/2018/05/To-Do-List-Excel-Template-01.png' />
       <input type="text" placeholder='add your task' onChange={(e)=>(setnewtask({name:e.target.value}))}/>
-      <input type="text" placeholder='add description' onChange={(e)=>(setnewtask({name:e.target.value}))}/>
       <button onClick={()=>fnc(newtask)}>Add task</button>
     </div>
   )
